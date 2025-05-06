0062c5071a11bef1ccf8de424ccabdcf22831ab1
cmeynet <clemence.meynet14@gmail.com>
2025-05-06 12:22:40 +0200

    create 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for lettre in str:
+        if ord('a') <= ord(lettre) <= ord('z'):
+            lettre = chr(ord(lettre) - 32)
+        print(f"{lettre}", end="")
+    print("")
