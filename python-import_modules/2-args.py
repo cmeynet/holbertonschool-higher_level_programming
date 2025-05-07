5f893dce8874bc3f5cdc048340dc7ec3f800183b
cmeynet <clemence.meynet14@gmail.com>
2025-05-07 22:44:06 +0200

    fix 2-args.py delete .format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
+    import sys
+
     arg_count = len(sys.argv) - 1
+
     if arg_count == 0:
-        print("{} arguments.".format(arg_count))
+        print("0 arguments.")
     elif arg_count == 1:
-        print("{} argument: ".format(arg_count))
+        print("1 argument:")
     else:
         print("{} arguments:".format(arg_count))
 
