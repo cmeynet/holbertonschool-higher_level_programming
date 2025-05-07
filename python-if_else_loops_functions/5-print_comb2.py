77816a844a4970315d8fcadc98893c98d3a826bb
cmeynet <clemence.meynet14@gmail.com>
2025-05-07 09:47:28 +0200

    fix 5-print_comb2 using one print

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print(f"{i:02d}", end=", ")
-    if i == 99:
-        print(f"{i:02d}")
+print(", ".join("{:02}".format(i) for i in range(100)))
