64936a3de10d6c8ca61d50c8b627c1a3a4e5b034
cmeynet <clemence.meynet14@gmail.com>
2025-05-19 16:21:20 +0200

    add description for 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Definition of an empty Square class. """
+
+
 class Square:
-    """ An empty class that defines a square """
+    """ An empty class that defines a square. """
     pass
