9193f5ca6d4c5c17ee88348df4d5e419c28fda69
cmeynet <clemence.meynet14@gmail.com>
2025-05-19 16:16:15 +0200

    create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Defines a square
+
+    Attributs :
+        size (int): size of the square
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new size
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
