b0af47c385b9778df7c992d20c77403fb1360550
cmeynet <clemence.meynet14@gmail.com>
2025-05-19 19:28:33 +0200

    create 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Definition of a Square class with type checking
+and method to calcul the area of the square  """
+
+
+class Square:
+    """
+    Defines a square and calculate its area
+
+    Attributs :
+        size (int): size of the square
+
+    Methods:
+        area: calculate the area of the square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a square with a size as an int
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Getter to access the size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter to modify size with verification """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Calculate the area of the current square
+
+        Returns:
+            The area of the current square
+        """
+        return self.__size * self.__size
