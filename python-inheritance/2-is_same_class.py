bd7c22b1f40d1ef49389729a7db8d4b861e79600
cmeynet <clemence.meynet14@gmail.com>
2025-05-26 16:50:00 +0200

    create 2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the "1-my_list" modulo.
+Its contains the function: print_sorted
+and the class: MyList
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that returns True if the object
+    is exactly an isntance of a specified class
+    """
+    if type(obj) is a_class:
+        return True
+    return False
