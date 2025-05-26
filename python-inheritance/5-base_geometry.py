cc3a33593e213ffca6362abc581dc3ba5e092c47
cmeynet <clemence.meynet14@gmail.com>
2025-05-26 21:43:30 +0200

    create 5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the "5-base_geometry" modulo.
+Its contains the class:
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ Empty class named BaseGeometry"""
+    pass
