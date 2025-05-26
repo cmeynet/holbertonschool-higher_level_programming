ca7a88b69edbe37a23bcdfc2022d2f00d266b30b
cmeynet <clemence.meynet14@gmail.com>
2025-05-26 12:07:00 +0200

    create README.md 0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the "O-lookup" modulo.
+Its contains the function:
+lookup
+"""
+
+
+def lookup(obj):
+    """
+    Function that returns a list
+    """
+    return dir(obj)
