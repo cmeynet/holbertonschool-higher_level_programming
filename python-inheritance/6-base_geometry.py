03604f3d3ecca8b0854a537a26b193aabe1eefbd
cmeynet <clemence.meynet14@gmail.com>
2025-05-26 21:54:53 +0200

    create 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is the "5-base_geometry" modulo.
+Its contains the class:
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Defines a BaseGeometry
+
+    Methods:
+        area: calculate the area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
