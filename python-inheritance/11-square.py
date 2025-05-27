de6a57b1ad12c69bd1c63ae7a622df687be37896
cmeynet <clemence.meynet14@gmail.com>
2025-05-27 15:01:15 +0200

    add integer_validator in 11-square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -62,6 +62,7 @@ class Square(Rectangle):
     """
     def __init__(self, size):
         super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
