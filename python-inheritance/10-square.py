90565834d2925c6b6c7b3878a9e7067731291c86
cmeynet <clemence.meynet14@gmail.com>
2025-05-30 10:07:22 +0200

    fix 11-square and 10-square delete spuer()

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -63,7 +63,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
 
     def area(self):
         return self.__size * self.__size
