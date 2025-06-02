bfb5f010d94c9ecfcd17b87a36b555d0a9c24a91
cmeynet <clemence.meynet14@gmail.com>
2025-06-02 12:21:04 +0200

    add comment for the function 1-write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" Function that writes a string to a text file
-and returns the number of characters written """
+""" It contains the function:
+    read_file """
 
 
 def write_file(filename="", text=""):
+    """ Function that writes a string to a text file
+    and returns the number of characters written """
     with open(filename, "w", encoding="utf-8") as file:
         file.write(text)
         return len(text)
