50b04102854fd836fcfedc7a64b4a3b0f5ab0b09
cmeynet <clemence.meynet14@gmail.com>
2025-06-02 12:18:39 +0200

    add comment for the function 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Function that reads a text file and prints it to stdout """
+""" It contains the function:
+    read_file """
 
 
 def read_file(filename=""):
+    """ Function that reads a text file and prints it to stdout """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
