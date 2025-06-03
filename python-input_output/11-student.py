b15c601a0e2b3bcd38b5a1279f768d42edb40f37
cmeynet <clemence.meynet14@gmail.com>
2025-06-03 14:58:13 +0200

    create 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" It contains the class:
+    Student """
+
+
+class Student:
+    """ Defines a Student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
+        if type(attrs) is list and all(type(attr) is str for attr in attrs):
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for key, value in json.items():
+            setattr(self, key, value)
