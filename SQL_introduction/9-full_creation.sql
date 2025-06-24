d99aad0c5f5c25c5bab27a9425c753de0ebd4758
cmeynet <clemence.meynet14@gmail.com>
2025-06-24 18:48:00 +0200

    create files from 9 to 16

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+--  create a table second_table in the database hbtn_0c_0
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256), 
+    score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8),
+(5, 'Aria', 18),
+(6, 'Aria', 12);
