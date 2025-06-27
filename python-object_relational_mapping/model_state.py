0e8ab05ecd11008c7f2cb27522329efd194d248c
cmeynet <clemence.meynet14@gmail.com>
2025-06-27 11:16:35 +0200

    fix model_state.py delete connexion in base

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,15 +9,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-engine = create_engine("mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa")
-
-Base.metadata.create_all(engine)
-
-
-
-
