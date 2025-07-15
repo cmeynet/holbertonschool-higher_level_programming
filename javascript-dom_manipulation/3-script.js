162e0ab1bf08bf398cd9e29e6b24197219478869
cmeynet <clemence.meynet14@gmail.com>
2025-07-15 11:59:37 +0200

    create 3-script.js

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,11 @@
+const toggleHeader = document.getElementById('toggle_header');
+
+const header = document.querySelector('header');
+
+toggleHeader.addEventListener('click', function () {
+  if (header.classList.contains('red')) {
+    header.classList.replace('red', 'green');
+  } else {
+    header.classList.replace('green', 'red');
+  }
+});
